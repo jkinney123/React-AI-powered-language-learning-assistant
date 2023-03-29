9d463b74731853dce7650dbd60c833c8c2684ff1
Joe Kinney <joekinney53@gmail.com>
2023-03-28 20:46:32 -0500

    first build of ai powered personalized translator.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,53 +10,35 @@ export default async function (req, res) {
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  const { name, target_language, user_input } = req.body;
+  const prompt = `Help ${name} learn ${target_language} by answering their question or providing a translation.\nUser: ${user_input}\n\nLanguage Assistant:`;
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: prompt,
+      max_tokens: 200,
+      temperature: 0.5,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+
+    const reply = response.data.choices[0].text.trim();
+    res.status(200).json({ reply });
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
-    } else {
+    } else
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
+    res.status(500).json({
+      error: {
+        message: 'An error occurred during your request.',
+      },
+    });
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,30 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [name, setName] = useState("");
+  const [targetLanguage, setTargetLanguage] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [response, setResponse] = useState("");
 
   async function onSubmit(event) {
     event.preventDefault();
+
     try {
-      const response = await fetch("/api/generate", {
+      const apiResponse = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ name, target_language: targetLanguage, user_input: userInput }),
       });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+      const data = await apiResponse.json();
+      if (apiResponse.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${apiResponse.status}`);
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
+      setResponse(data.reply);
+    } catch (error) {
       console.error(error);
       alert(error.message);
     }
@@ -34,24 +35,43 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Language Learning Assistant</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h1>Welcome to the Personalized Language Learning Assistant!</h1>
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
+          <label>
+            What's your name?
+            <input
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Which language would you like to learn?
+            <input
+              type="text"
+              value={targetLanguage}
+              onChange={(event) => setTargetLanguage(event.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Ask a question or request a translation:
+            <input
+              type="text"
+              value={userInput}
+              onChange={(event) => setUserInput(event.target.value)}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {response && <p>Assistant: {response}</p>}
       </main>
     </div>
   );
